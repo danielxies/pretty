7ecfacc6b25f52f3a3354caf1c519721428a13c6
danielemporia <xie476@purdue.edu>
2024-09-28 00:12:41 -0400

    shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/icon.png" type="image/png" />
+        <link rel="icon" href="icon.png" type="image/png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
--- components/textarea.tsx
+++ components/textarea.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
